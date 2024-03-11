73f3c6ad664d0ce71ed0347b614f26aa03337905
lukas.smicys <lukas.smicys@ext.fmc-ag.com>
2024-03-12 01:02:43 +0200

    @Data deals with boiler plates.

--- spring-boot-15-puzzle/src/main/java/com/example/springboot15puzzle/dto/MoveRequest.java
+++ spring-boot-15-puzzle/src/main/java/com/example/springboot15puzzle/dto/MoveRequest.java
@@ -5,12 +5,4 @@ import lombok.Data;
 @Data
 public class MoveRequest {
     private int tileValue;
-
-    public int getTileValue() {
-        return tileValue;
-    }
-
-    public void setTileValue(int tileValue) {
-        this.tileValue = tileValue;
-    }
 }
